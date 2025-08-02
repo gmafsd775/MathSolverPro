aa9ea5a977e92ce9ff8d8879f62628f613bca417
gmafsd775 <gmafsd775@gmail.com>
2025-08-02 13:46:54 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MathSolverPro/', // 🔥 Ye line add karni hai
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
